1b0092bd9b61e23321814b9a7c21b231dadb29ef
iamoperand <narora200@gmail.com>
2018-09-25 04:12:49 +0530

    [Feat]: Serverless deployed :)

--- handler.js
+++ handler.js
@@ -1,20 +1,7 @@
 "use strict";
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
 module.exports.sendReminderDaily = (event, context, callback) => {
-  const aws = require("aws-sdk");
+  const AWS = require("aws-sdk");
   AWS.config.update({ region: "us-east-1" });
 
   const ses = new AWS.SES();
@@ -22,7 +9,50 @@ module.exports.sendReminderDaily = (event, context, callback) => {
 
   const emailHtml = fs.readFileSync("./dailyReminder.html", "utf-8");
 
-  const toAndFromAddress = "verifiedemail@yourdomain.com";
+  const toAndFromAddress = "niksataws@gmail.com";
+  const params = {
+    Destination: {
+      ToAddresses: [toAndFromAddress]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: emailHtml
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "Remember to continue helping the Woof Garden!"
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Woof Grden Reminder"
+      }
+    },
+    ReplyToAddresses: [toAndFromAddress],
+    Source: toAndFromAddress
+  };
+
+  ses.sendEmail(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+  const AWS = require("aws-sdk");
+  AWS.config.update({ region: "us-east-1" });
+
+  const ses = new AWS.SES();
+  const fs = require("fs");
+
+  const emailHtml = fs.readFileSync("./weekendReminder.html", "utf-8");
+
+  const toAndFromAddress = "niksataws@gmail.com";
   const params = {
     Destination: {
       ToAddresses: [toAndFromAddress]
